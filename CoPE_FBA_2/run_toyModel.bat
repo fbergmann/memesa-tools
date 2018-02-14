8bae4539846113ce0a70f658e97688164eb67eb9
Frank Bergmann <fbergman@caltech.edu>
2018-02-14 15:09:50 +0100

    - parameterize batch file

--- CoPE_FBA_2/run_toyModel.bat
+++ CoPE_FBA_2/run_toyModel.bat
@@ -10,24 +10,29 @@ SET JAVA_DIR=C:\Program Files\Java\jdk1.8.0_131\bin
 
 SET PATH=%PYTHON_DIR%;%BASH_DIR%;%JAVA_DIR%;%PATH%
 SET CLASSPATH=%BASE_DIR%/rational_fba;%BASE_DIR%/rational_fba/jlinalg_0.5.jar;%CLASSPATH%
+SET MODEL=toy_model
+SET INFINITY=1000
+SET LEVEL=3
 
 echo P1
-python P1_model_setup.py toy_model
+python P1_model_setup.py %MODEL%
 
 echo P2
-python P2_find_duplicate_reactions.py -m toy_model -b 1000 -l 3
+python P2_find_duplicate_reactions.py -m %MODEL% -b %INFINITY% -l %LEVEL%
 
 echo P3
-python P3_get_F_modules.py -m toy_model -l 3
+python P3_get_F_modules.py -m %MODEL% -l %LEVEL%
 
 echo P4
-python P4_SBML2H-format.py -m toy_model -b 1000 -l 3
+python P4_SBML2H-format.py -m %MODEL% -b %INFINITY% -l %LEVEL%
 
 echo P5
-sh ./P5_do_rational_FBA.sh toy_model
+cd rational_fba
+sh ./rational_FBAw.sh %MODEL%
+cd ..
 
 echo P6
-python P6_stripModel2Modules.py -m toy_model -b 1000 -l 3
+python P6_stripModel2Modules.py -m %MODEL% -b %INFINITY% -l %LEVEL%
 
 
 :: restore path 
